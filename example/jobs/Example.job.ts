f2e6846949668164c71177bbaad3d6e7d023823e
David De Anda <omarandstuff@gmail.com>
2023-09-28 17:55:28 -0600

    Remove example files

--- example/jobs/Example.job.ts
+++
@@ -1,8 +0,0 @@
-import { BaseJob } from '@universal-packages/background-jobs'
-
-export default class ExampleJob extends BaseJob<{ hola: string }> {
-  public static schedule = { cronTime: '* * * * *' }
-  public async perform(payload: any) {
-    console.log(payload)
-  }
-}
--- example/modules/worker.app.ts
+++
@@ -1,1 +0,0 @@
-export { default } from '../../src/Worker.universal-core-app'
